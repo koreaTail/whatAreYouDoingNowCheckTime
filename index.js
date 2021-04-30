0d65acad9a6dea718cf8f8e7b0db3064c6cea551
leesangjun <prokjun@gmail.com>
2021-04-30 21:13:30 +0900

    얼추 완성. 좋아.. ㅋㅋ

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 const btnSection = document.querySelector(".section-flex")
 const 시계위치 = document.querySelector(".시계위치")
-
-const p = document.createElement("p")
-시계위치.appendChild(p)
+const p = 시계위치.querySelector("p")
 
 
 btnSection.addEventListener("submit", btnHandler)
 
 function btnHandler() {
   event.preventDefault();
-  const 시 = new Date().getHours()
-  const 분 = new Date().getMinutes()
-  const 현재시각 = `${
-    if (시 < 10) {
-    "0" + 시
-  } else {
-    시
-  }
-   : ${
-    if (분 < 10) {
-      "0" + 분
-    } else {
-      분
-    }
-  } `
+
 
   const 해당섹션 = event.target.parentElement;
   const 경과시간표시부 = 해당섹션.querySelector(".경과시간표시부")
@@ -34,31 +18,44 @@ function btnHandler() {
     const span = document.createElement("span")
     경과시간표시부.appendChild(li)
     li.appendChild(span)
-    span.innerHTML = `${ 현재시각 } - `
+    span.innerHTML = `${현재시간()} - `
   }
   else {
     const li = 해당섹션.querySelectorAll("li")
     const lastLi = li[li.length - 1]
     const span = document.createElement("span")
+    const span2 = document.createElement("span")
     lastLi.appendChild(span)
-    span.innerText = 현재시각
+    lastLi.appendChild(span2)
+    span.innerText = 현재시간();
+    const 처음과끝시간 = lastLi.textContent;
+    const 경과시간 = (처음과끝시간.slice(0, 2) - 처음과끝시간.slice(8, 10)) * 60 + (처음과끝시간.slice(3, 5) - 처음과끝시간.slice(11, 13));
+    span2.innerText = ` (${경과시간}')`;
   }
-
-
-  // console.log(parseInt(현재시각()))
-  // console.log(parseInt(시작시각))
 }
 
+function 현재시간() {
+  const 시 = new Date().getHours()
+  const 분 = new Date().getMinutes()
 
+  if (시 < 10 && 분 < 10) {
+    return `0${시}:0${분}`;
+  } else if (시 < 10 && 분 >= 10) {
+    return `0${시}:${분}`;
+  } else if (시 >= 10 && 분 < 10) {
+    return `${시}:0${분}`;
+  } else if (시 >= 10 && 분 >= 10) {
+    return `${시}:${분}`;
+  }
+}
 
-console.log(시계())
 
 function 시계() {
   const 시 = new Date().getHours()
   const 분 = new Date().getMinutes()
   const 초 = new Date().getSeconds()
 
-  return `${ 시 }: ${ 분 }: ${ 초 } `
+  p.innerText = `${시}: ${분}: ${초} `
 }
 
 setInterval(시계, 1000)
